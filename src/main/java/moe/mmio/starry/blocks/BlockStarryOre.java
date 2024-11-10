189220236c5897e79c679b727478e5dacdbbdb6b
MinimalMio <yukati2024@outlook.com>
2024-11-10 18:20:03 +0800

    [block]: 幻星矿

---
+++ src/main/java/moe/mmio/starry/blocks/BlockStarryOre.java
@@ -0,0 +1,86 @@
+package moe.mmio.starry.blocks;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import moe.mmio.starry.StarryMod;
+import moe.mmio.starry.items.ModItems;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+
+// 是本模组第一个方块，放些笔记在这里
+public class BlockStarryOre extends Block {
+
+
+    /*
+    // 如果是需要定义多面，用这个，这里只需要定义一面(备注以便后面开发)
+    private IIcon[] sideIcons;  // 侧面纹理列表
+
+    private IIcon topIcon;      // 顶部纹理
+
+    private IIcon bottomIcon;   // 底部纹理
+    */
+
+    private IIcon Icon;
+
+    public BlockStarryOre(Material material) {
+        super(material.iron);        // 材质:铁块
+        this.setHardness(25.0f);
+        this.setResistance(15.0f);
+        this.setBlockName("starry_ore");
+        this.setBlockTextureName(StarryMod.MODID + ":starry_ore");
+        this.setHarvestLevel("pickaxe", 2);   // 设置挖掘等级,木头/金:0,石头:1,铁:2,钻石:3
+
+        /*
+        // 其他的一些未来可能会用到的属性
+        this.setLightLevel(0f);            // 光照等级，范围:0f~1.0f
+        this.setLightOpacity(0);           // 透光程度，最高值255(完全不透光)
+        this.setBlockUnbreakable();        // 使其不可破坏
+        */
+    }
+
+    /*
+    // 多纹理示例代码
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister) {
+        this.topIcon = iconRegister.registerIcon(this.getTextureName() + "_top");
+        this.bottomIcon = iconRegister.registerIcon(this.getTextureName() + "_bottom");
+        this.sideIcons = new IIcon[4];
+        this.sideIcons[0] = iconRegister.registerIcon(this.getTextureName() + "side_0");
+        this.sideIcons[1] = iconRegister.registerIcon(this.getTextureName() + "side_1");
+        this.sideIcons[2] = iconRegister.registerIcon(this.getTextureName() + "side_2");
+        this.sideIcons[3] = iconRegister.registerIcon(this.getTextureName() + "side_3");
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side, int meta) {
+        if ( side == 0 ) {
+            return this.bottomIcon;             // 底部纹理
+        } else if ( side == 1 ) {
+            return this.topIcon;                // 顶部纹理
+        } else if ( side >= 2 && side <= 5 ) {
+            return this.sideIcons[side - 2];    // 四个侧面的纹理
+        } else {
+            return this.blockIcon;              // 当面的数字大于5时，返回默认纹理
+        }
+    }
+    */
+
+    @Override
+    public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune) {
+        ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
+
+        // 添加掉落物
+        drops.add(new ItemStack(ModItems.starry_ingot, 1));
+
+        return drops;
+    }
+
+}
--- src/main/java/moe/mmio/starry/items/ModItems.java
+++ src/main/java/moe/mmio/starry/items/ModItems.java
@@ -2,6 +2,9 @@ package moe.mmio.starry.items;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import moe.mmio.starry.StarryMod;
+import moe.mmio.starry.blocks.BlockStarryOre;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
@@ -13,11 +16,17 @@ public class ModItems {
 
     public static Item starry_ingot;
 
+    public static Block starry_ore;
+
     public static void initializeItem() {
         starry_ingot = new Item().setUnlocalizedName("starry_ingot").setMaxStackSize(64).setCreativeTab(CreativeTabs.tabMisc).setTextureName(StarryMod.MODID + "starry_ingot");
+
+        starry_ore = new BlockStarryOre(Material.iron).setCreativeTab(CreativeTabs.tabBlock);
     }
 
     public static void registerItems() {
         GameRegistry.registerItem(starry_ingot, starry_ingot.getUnlocalizedName());
+
+        GameRegistry.registerBlock(starry_ore, starry_ore.getUnlocalizedName());
     }
 }
