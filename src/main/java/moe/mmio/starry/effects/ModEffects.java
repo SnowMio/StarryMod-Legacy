0b5848369ebd860ee01a2788345fa091edfc4231
MinimalMio <yukati2024@outlook.com>
2024-11-13 01:33:20 +0800

    [fix]: forge i love u x2

--- src/main/java/moe/mmio/starry/effects/ModEffects.java
+++ src/main/java/moe/mmio/starry/effects/ModEffects.java
@@ -3,6 +3,6 @@ package moe.mmio.starry.effects;
 public class ModEffects {
     public static EffectLegendResurrection legendResurrection;
     public static void initEffects() {
-        legendResurrection = new EffectLegendResurrection(32, false, 0xFFFFFF);
+        legendResurrection = new EffectLegendResurrection(31, false, 0xFFFFFF);
     }
 }
