4378a4a43a24ca4139aebc95e810405ca3295e99
MinimalMio <yukati2024@outlook.com>
2024-11-13 01:35:18 +0800

    [fix]: forge i love u x3

--- src/main/java/moe/mmio/starry/effects/ModEffects.java
+++ src/main/java/moe/mmio/starry/effects/ModEffects.java
@@ -3,6 +3,8 @@ package moe.mmio.starry.effects;
 public class ModEffects {
     public static EffectLegendResurrection legendResurrection;
     public static void initEffects() {
+        // 在1.7.10中，药水id的分配最高上限是31超过这个值会导致崩溃
+        // 你她喵的还不支持动态分配
         legendResurrection = new EffectLegendResurrection(31, false, 0xFFFFFF);
     }
 }
